4b8ee1447350315eb874e0055e664bab02ba2501
ashwini-8 <ashwinip309@gmail.com>
2021-01-13 21:50:21 +0530

    Added: Ability to load the CSV files and ensure the number of records matches

---
+++ __test__/CensusAnalyser.test.js
@@ -0,0 +1,16 @@
+const { test, expect } = require('@jest/globals')
+const CensusAnalyser = require('../main/CensusAnalyser')
+const INDIA_STATE_CENSUS_CSV = "C:\\Users\\ashwi\\Desktop\\Bridgelabz\\JavaScript\\Assignments\\IndiaStateCensusAnalyserProblem\\main\\IndiaStateCensusData.csv"
+
+/*test('record_mathes',() => { 
+    const censusAnalyser = new CensusAnalyser()
+    return censusAnalyser.indiaStateAnalyser(INDIA_STATE_CENSUS_CSV).then(data =>expect(data.length).toBe(29))
+    
+})*/
+
+describe('testsForLoadIndiaStateCensus_CSV', () => {
+    test('givenStateCensusFile_EnsureNoOfRecordsMatches_WithTheFile', () => {
+        const censusAnalyzer = new CensusAnalyzer(INDIA_STATE_CENSUS_CSV);
+        return censusAnalyser.indiaStateAnalyser(INDIA_STATE_CENSUS_CSV).then(data =>expect(data.length).toBe(29))
+    });
+}); 
