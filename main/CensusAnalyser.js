0cfe300155ca5b6dca5789c8c9b1bab884e45dc0
ashwini-8 <ashwinip309@gmail.com>
2021-01-13 22:00:02 +0530

    Added: ensure the number of records matches and Happy test case is passed

--- main/CensusAnalyser.js
+++ main/CensusAnalyser.js
@@ -3,23 +3,17 @@ const fs = require('fs')
 let result = []
 class CensusAnalyser {
   constructor() {}
-
     indiaStateAnalyser(CSV_DATA) {
-      console.log(CSV_DATA)
     return new Promise((resolve, reject) => {
       fs.createReadStream(CSV_DATA)
         .pipe(csv())
         .on('data', (data) => result.push(data))
         .on('end', () => {
           resolve(result)
-          error => reject(error )
+          error => reject(error)
     })
   })
-
   }
-  //indiaStateAnalyser()
-  //.then(getresult => console.log(getresult))
-  //.catch(error => console.log(error));
 }
 module.exports = CensusAnalyser;
 
